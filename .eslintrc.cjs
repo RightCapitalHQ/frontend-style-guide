220399b334cf70be512f17e016419fdfdf8c06fe
frantic1048 <i@frantic1048.com>
2023-09-20 15:07:51 +0800

    ci: refine renovate-auto-changefile logic

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,18 +4,23 @@ module.exports = {
   overrides: [
     {
       // source files
-      files: ['packages/**/*.{js,cjs}'],
+      files: ['packages/**/*.{js,cjs,mjs}'],
       extends: '@rightcapital/javascript',
       env: { commonjs: true },
     },
     {
       // config files, scripts
-      files: ['./*.{js,cjs}', 'scripts/**/*.{js,cjs}'],
+      files: ['./*.{js,cjs,mjs}', 'scripts/**/*.{js,cjs,mjs}'],
       extends: '@rightcapital/javascript',
       env: { node: true, commonjs: true },
       rules: {
+        'no-unused-expressions': ['error', { allowTaggedTemplates: true }],
         'import/no-extraneous-dependencies': 'off',
       },
+    },
+    {
+      files: ['./*.mjs', 'scripts/**/*.mjs'],
+      env: { node: true, commonjs: false },
     },
   ],
 };
