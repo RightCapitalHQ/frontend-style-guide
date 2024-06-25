58d1e2edd72997f93aa17291085eb797311fab23
Bryan Lee <38807139+liby@users.noreply.github.com>
2024-06-25 15:07:41 +0800

    refactor!: migrate from eslint-plugin-react to eslint-react

--- packages/eslint-config-typescript-react/src/index.ts
+++ packages/eslint-config-typescript-react/src/index.ts
@@ -4,25 +4,18 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 
 const config: Linter.Config = {
   extends: [
-    require.resolve('eslint-config-airbnb/rules/react'),
-    require.resolve('eslint-config-airbnb/rules/react-a11y'),
-    require.resolve('eslint-config-airbnb/hooks'),
     require.resolve('@rightcapital/eslint-config-typescript'),
+    require.resolve('eslint-config-airbnb/hooks'),
+    require.resolve('eslint-config-airbnb/rules/react-a11y'),
     'plugin:@rightcapital/recommended-react',
+    'plugin:@eslint-react/recommended-legacy',
   ],
   plugins: ['@rightcapital'],
   reportUnusedDisableDirectives: true,
   rules: {
     'jsx-a11y/anchor-is-valid': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
-    'react/function-component-definition': 'off',
-    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.tsx'] }],
-    'react/jsx-key': ['error'],
-    'react/jsx-no-useless-fragment': ['error', { allowExpressions: true }],
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-    // We are using TypeScript, so it's not necessary to use React's defaultProps
-    'react/require-default-props': 'off',
+    '@eslint-react/no-useless-fragment': ['error'],
   },
 };
 
